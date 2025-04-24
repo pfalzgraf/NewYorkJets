896d3c3433c693283bc2f660a2d1e2b4013a65e5
Nicholas Pfalzgraf <153227910+NickPfalzgraf@users.noreply.github.com>
2025-04-24 18:38:01 -0500

    Updated

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d161bef9a794230ccc049e4e618b53aaae4309e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+962600a0a029dc3f0e246e6a41d13ad5cb4fc429")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
