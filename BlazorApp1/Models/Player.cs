c3ec48373dc591c9445abffd7d66b86ad2731818
Nicholas Pfalzgraf <153227910+NickPfalzgraf@users.noreply.github.com>
2025-04-24 15:18:16 -0500

    Initial commit

---
+++ BlazorApp1/Models/Player.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp1.Models;
+
+public class Player
+{
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string College { get; set; }
+    public string Position { get; set; }
+    public string OverallRank { get; set; }
+}
